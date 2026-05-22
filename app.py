7ec5ef43a4bd11d1a964e11efc894b270d64c923
FreysiY <freysi345@gmail.com>
2026-05-21 21:10:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,35 @@ if seccion == "Home":
 
 elif seccion == "Ejercicio 1":
           st.title("Flujo de Caja con Listas")
-          st.markdown("Registro de ingresos y gastos usando listas")
+          st.markdown("Registro de ingresos y gastos usando listas → diccionario → DataFrame")
+          
+          if "concepto" not in st.session_state:
+                    st.session_state.concepto = []
+                    
+          if "tipodemovimiento" not in st.session_state:
+                    st.session_state.tipodemovimiento = []
+          
+          if "valor" not in st.session_state:
+                    st.session_state.valor = []
+
+          concepto = st.text_input("Concepto")
+          tipodemovimiento = st.selectbox("Tipo de movimiento", ["Ingreso", "Gasto"])
+          valor = st.number_input("Valor", min_value=0.0)
+          
+          if st.button("Agregar movimiento"):
+                    
+                    st.session_state.concepto.append(concepto)
+                    st.session_state.tipodemovimiento.append(tipodemovimiento)
+                    st.session_state.valor.append(valor)
+          
+                    st.success("Movimiento agregado")
+         
+          if st.session_state.concepto:
+                    datos = {
+                                "Concepto": st.session_state.concepto,
+                                "Tipo de movimiento": st.session_state.tipodemovimiento,
+                                "Valor": st.session_state.valor
+                    }
+                    
+                    df = pd.DataFrame(datos)
+                    
